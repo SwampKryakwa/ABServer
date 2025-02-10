7a2bc219a6eb3d8b4c00c3da1efd05a9a1a71cf4
SwampKryakwa <swampkryakwa@yandex.ru>
2025-02-10 19:19:36 +0300

    Start quicksand freeze

---
+++ AB Server/Gates/QuicksandFreeze.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AB_Server.Gates
+{
+    class QuicksandFreeze : GateCard
+    {
+        public override void DetermineWinner()
+        {
+            if (!Negated)
+                Open();
+            else
+                base.DetermineWinner();
+        }
+
+        public override void Open()
+        {
+            base.Open();
+        }
+
+        public override bool IsOpenable() =>
+            false;
+    }
+}

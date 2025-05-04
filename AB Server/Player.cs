38e849f0e065b6981abceb53689afab860148ce4
SwampKryakwa <swampkryakwa@yandex.ru>
2025-05-04 08:40:13 +0300

    Fix bakugan grave not being created

--- AB Server/Player.cs
+++ AB Server/Player.cs
@@ -54,16 +54,6 @@ namespace AB_Server
             BakuganGrave = new(this);
         }
 
-        public Player(byte id, byte sideID, Game game, string displayName, byte avatar)
-        {
-            Id = id;
-            TeamId = sideID;
-            this.Game = game;
-            BakuganGrave = new(this);
-            DisplayName = displayName;
-            Avatar = avatar;
-        }
-
         public void ProvideDeck(dynamic deck)
         {
             if (deck["deck_color"] != null)
@@ -145,94 +135,6 @@ namespace AB_Server
                 Game.GateIndex.Add(gate);
             }
         }
-
-        public static Player FromJson(byte id, byte sideID, dynamic deck, Game game, string displayName, byte avatar)
-        {
-            Player player = new(id, sideID, game, displayName, avatar);
-
-            //player.DisplayName = deck["dispName"].ToString();
-
-            if (deck["deck_color"] != null)
-                player.PlayerColor = deck["deck_color"];
-            else
-                player.PlayerColor = 0;
-
-            foreach (dynamic b in deck["bakugans"])
-            {
-                int type = (int)b["Type"];
-                short power = (short)b["Power"];
-                int attr = (int)b["Attribute"];
-                int treatment = (int)b["Treatment"];
-
-                Bakugan bak = new((BakuganType)type, power, (Attribute)attr, (Treatment)treatment, player, game, game.BakuganIndex.Count);
-                game.BakuganIndex.Add(bak);
-                player.Bakugans.Add(bak);
-                player.BakuganOwned.Add(bak);
-            }
-
-            foreach (int a in deck["abilities"])
-            {
-                AbilityCard abi = AbilityCard.CreateCard(player, game.AbilityIndex.Count, a);
-                player.AbilityHand.Add(abi);
-                game.AbilityIndex.Add(abi);
-            }
-
-            FusionAbility fusion = FusionAbility.FusionCtrs[deck["bakugans"][0]["Type"]].Invoke(game.AbilityIndex.Count, player);
-            player.AbilityHand.Add(fusion);
-            game.AbilityIndex.Add(fusion);
-
-            player.BakuganOwned[0].IsPartner = true;
-
-            if (deck.ContainsKey("correlation"))
-            {
-                var correlation = AbilityCard.CorrelationCtrs[deck["correlation"]].Invoke(game.AbilityIndex.Count, player);
-                player.AbilityHand.Add(correlation);
-                game.AbilityIndex.Add(correlation);
-            }
-            else
-            {
-                HashSet<Attribute> combinedAttributes = new(player.BakuganOwned.Select(x => x.MainAttribute));
-                if (combinedAttributes.SetEquals(new HashSet<Attribute> { Attribute.Nova, Attribute.Lumina, Attribute.Aqua }) ||
-                       combinedAttributes.SetEquals(new HashSet<Attribute> { Attribute.Zephyros, Attribute.Subterra, Attribute.Darkon }))
-                {
-                    TripleNode tripleNode = new(game.AbilityIndex.Count, player);
-                    game.AbilityIndex.Add(tripleNode);
-                    player.AbilityHand.Add(tripleNode);
-                }
-                else if ((combinedAttributes.Contains(Attribute.Nova) && combinedAttributes.Contains(Attribute.Darkon)) ||
-                       (combinedAttributes.Contains(Attribute.Subterra) && combinedAttributes.Contains(Attribute.Aqua)) ||
-                       (combinedAttributes.Contains(Attribute.Lumina) && combinedAttributes.Contains(Attribute.Zephyros)))
-                {
-                    DiagonalCorrelation diagonalCorrelation = new(game.AbilityIndex.Count, player);
-                    game.AbilityIndex.Add(diagonalCorrelation);
-                    player.AbilityHand.Add(diagonalCorrelation);
-                }
-                else if (combinedAttributes.Distinct().Count() == 1)
-                {
-                    ElementResonance elementResonance = new(game.AbilityIndex.Count, player);
-                    game.AbilityIndex.Add(elementResonance);
-                    player.AbilityHand.Add(elementResonance);
-                }
-                else
-                {
-                    AdjacentCorrelation adjacentCorrelation = new(game.AbilityIndex.Count, player);
-                    game.AbilityIndex.Add(adjacentCorrelation);
-                    player.AbilityHand.Add(adjacentCorrelation);
-                }
-            }
-
-            foreach (dynamic g in deck["gates"])
-            {
-                GateCard gate;
-                gate = GateCard.CreateCard(player, game.GateIndex.Count, (int)g["Type"]);
-
-                player.GateHand.Add(gate);
-                game.GateIndex.Add(gate);
-            }
-
-            return player;
-        }
-
         public bool HasThrowableBakugan() =>
             Bakugans.Count != 0 && Game.Field.Cast<GateCard>().Any(x => x != null) && !HadThrownBakugan;
 

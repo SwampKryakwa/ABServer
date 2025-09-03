d8451c8a314396d00012b5bffb52e62c2a1c0f55
SwampKryakwa <swampkryakwa@yandex.ru>
2025-09-03 16:47:32 +0300

    Fix Slash Zero

--- AB Server/Abilities/SlashZero.cs
+++ AB Server/Abilities/SlashZero.cs
@@ -5,12 +5,12 @@ namespace AB_Server.Abilities
     internal class SlashZero(int cId, Player owner, int typeId) : AbilityCard(cId, owner, typeId)
     {
         public override void TriggerEffect() =>
-            User.Boost(new Boost((short)(80 * Game.AbilityIndex.Count(x => x.Owner == Owner))), this);
+            User.Boost(new Boost((short)(80 * Owner.AbilityDrop.Count)), this);
 
         public override bool IsActivateableByBakugan(Bakugan user) =>
             Game.CurrentWindow == ActivationWindow.Normal && user.Type == BakuganType.Shredder && user.InBattle;
 
         [ModuleInitializer]
-        internal static void Init() => AbilityCard.Register(36, CardKind.NormalAbility, (cID, owner) => new SlashZero(cID, owner, 36));
+        internal static void Init() => Register(36, CardKind.NormalAbility, (cID, owner) => new SlashZero(cID, owner, 36));
     }
 }

1e87c7e757ba8ad750156d34a4d336040e41760a
SwampKryakwa <swampkryakwa@yandex.ru>
2025-09-20 14:48:50 +0300

    Fix Negative Delta

--- AB Server/Gates/NegativeDelta.cs
+++ AB Server/Gates/NegativeDelta.cs
@@ -14,8 +14,8 @@
 
         public override void Resolve()
         {
-            if (Bakugans.Any(x => x.BaseAttribute == Attribute.Darkon || x.BaseAttribute == Attribute.Subterra || x.BaseAttribute == Attribute.Zephyros))
-                foreach (var bakugan in Bakugans.Where(x => x.BaseAttribute == Attribute.Darkon || x.BaseAttribute == Attribute.Subterra || x.BaseAttribute == Attribute.Zephyros))
+            if (Bakugans.Any(x => x.IsAttribute(Attribute.Nova) || x.IsAttribute(Attribute.Aqua) || x.IsAttribute(Attribute.Lumina)))
+                foreach (var bakugan in Bakugans.Where(x => x.IsAttribute(Attribute.Nova) || x.IsAttribute(Attribute.Aqua) || x.IsAttribute(Attribute.Lumina)))
                     bakugan.Boost(new Boost(-200), this);
             else
                 foreach (var bakugan in Bakugans)

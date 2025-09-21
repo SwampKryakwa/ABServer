5a66e76e9a199ed79c4eccd66d2ad73bccc04de7
SwampKryakwa <swampkryakwa@yandex.ru>
2025-09-21 12:22:07 +0300

    Gaze of Exedra

---
+++ AB Server/Abilities/GazeOfExedra.cs
@@ -0,0 +1,32 @@
+using System.Runtime.CompilerServices;
+
+namespace AB_Server.Abilities
+{
+    internal class GazeOfExedra : AbilityCard
+    {
+        public GazeOfExedra(int cID, Player owner, int typeId) : base(cID, owner, typeId)
+        {
+            CondTargetSelectors =
+            [
+                new BakuganSelector() { ClientType = "BH", ForPlayer = (p) => p == Owner, Message = "INFO_ABILITY_TARGET", TargetValidator = x => x.IsAttribute(Attribute.Darkon) && x.InHand() && x.Owner == Owner }
+            ];
+        }
+
+        public override void TriggerEffect()
+        {
+            var target = (CondTargetSelectors[0] as BakuganSelector)!.SelectedBakugan;
+            if (target.InHand())
+            {
+                target.MoveFromHandToDrop();
+                User.Boost(200, this);
+            }
+        }
+
+        public override bool IsActivateableByBakugan(Bakugan user) =>
+            Game.CurrentWindow == ActivationWindow.Normal && user.OnField();
+
+        [ModuleInitializer]
+        internal static void Init() => Register(50, CardKind.NormalAbility, (cID, owner) => new GazeOfExedra(cID, owner, 50));
+    }
+}
+

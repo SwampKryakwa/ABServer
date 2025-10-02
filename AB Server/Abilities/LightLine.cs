9d5ef5d65bfce2610745ca11fd7aac266790a6d7
SwampKryakwa <swampkryakwa@yandex.ru>
2025-10-02 15:35:21 +0300

    Fix lightline missing

---
+++ AB Server/Abilities/LightLine.cs
@@ -0,0 +1,30 @@
+using AB_Server.Gates;
+using System.Linq;
+
+namespace AB_Server.Abilities
+{
+    internal class LightLine : AbilityCard
+    {
+        public LightLine(int cID, Player owner, int typeId) : base(cID, owner, typeId) { }
+
+        public override void TriggerEffect()
+        {
+            // Get all Haos Bakugan on the field owned by this card's owner
+            var haosBakugan = Game.BakuganIndex
+                .Where(b => b.OnField() && b.Owner == Owner && b.IsAttribute(Attribute.Lumina))
+                .ToArray();
+
+            var numMyBakuganNotUserOnField = Game.BakuganIndex.Where(b => b.OnField() && b.Owner == Owner && b != User).Count();
+
+            foreach (var bak in haosBakugan)
+                bak.Boost(100 * numMyBakuganNotUserOnField, this);
+        }
+
+        public override bool IsActivateableByBakugan(Bakugan user) =>
+            user.OnField() && user.IsAttribute(Attribute.Lumina);
+
+        [System.Runtime.CompilerServices.ModuleInitializer]
+        internal static void Init() =>
+            Register(53, CardKind.NormalAbility, (cID, owner) => new LightLine(cID, owner, 53));
+    }
+}
